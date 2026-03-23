ac9f0015c1a0fd9d9fefeff3c0a4ecac0b3e7c05
kunal kumar <kunalkr620593@gmail.com>
2026-03-23 20:22:42 +0530

    add

---
+++ check_character_entered_by_user_upperCase_or_lowercase_or_other_type.c
@@ -0,0 +1,21 @@
+/*Write a program to determine whether a character entered by the user is lowercase or upper Case or special character or not character.*/
+#include<stdio.h>
+int main(){
+char ch;
+printf("Enter a character=");
+    scanf("%c",&ch);
+    if(ch>='A' && ch<='Z'){ //compare if character is UpperCase
+        printf("Entered character is Uppercase character.");
+    }
+    else if(ch>='a' && ch<='z'){ //Compare if character is Lowercase
+        printf("Entered character is Lowercase character.");
+    }
+    else if(ch>='0' && ch<='9'){ //compare if character is not character
+        printf("Entered character is Digit (not character)");
+            
+    }
+    else{ //If entered character is special character
+        printf("Entered character is Special character.");  
+    }
+    return 0;
+}
