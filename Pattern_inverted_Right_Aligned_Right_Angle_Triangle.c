c9db9da34b35c5f230a0db5e4dc0443e1dab48f9
kunal kumar <kunalkr620593@gmail.com>
2026-04-12 20:42:20 +0530

    new

---
+++ Pattern_inverted_Right_Aligned_Right_Angle_Triangle.c
@@ -0,0 +1,24 @@
+/*      Inverted Right-Aligned Right angle Triangle Shape 
+
+* * * * *
+  * * * *
+    * * *
+      * *
+        *              */
+
+#include<stdio.h>
+int main(){
+int n;
+printf("Enter number of row=");
+scanf("%d",&n);
+for(int i=n;i>=1;i--){
+    for(int j=1;j<=(n-i);j++){
+        printf("  ");
+    }
+    for(int k=1;k<=i;k++){
+        printf("* ");
+    }
+    printf("\n");
+}
+    return 0;
+}
